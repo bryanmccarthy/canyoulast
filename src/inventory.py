799b68fbe394e409684efc8fee7b48d1c9b8688d
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-07-11 19:47:31 -0400

    add slot for weapon

--- src/inventory.py
+++ src/inventory.py
@@ -6,7 +6,13 @@ class Inventory(pygame.sprite.Sprite):
     self.slot_size = 48
     self.items = pygame.sprite.Group()
 
+    image = pygame.image.load('assets/heroes/knight/weapon_sword_1.png').convert_alpha()
+    self.image = pygame.transform.scale(image, (48, 48))
+
   def render(self, screen):
+    pygame.draw.rect(screen, (40, 40, 40), pygame.Rect(345, 700, 48, 48))
+    screen.blit(self.image, (345, 700))
+
     # x pos + (2*i) to get spacing, + 408 to center
     for i in range(8):
       pygame.draw.rect(screen, (40, 40, 40), pygame.Rect(i * self.slot_size + (2*i) + 408, 700, 48, 48))
